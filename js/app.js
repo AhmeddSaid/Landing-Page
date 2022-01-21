19c1cfff1e8dbd530a559aaa3d30126a2303fcc6
AhmeddSaid <AhmeddSaid@users.noreply.github.com>
2022-01-21 13:34:10 +0200

    Issue resolved
    -fixed getBoundingClientRect() bug

--- js/app.js
+++ js/app.js
@@ -1,77 +1,81 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
- * 
-*/
-const navBarMenu = document.getElementById('navbar__list'); // empty unordered list
-const navBarMenuSections = [...document.querySelectorAll('section')]; // creates an array and add items to it for each section in the document
+ *
+ */
+const navBarMenu = document.getElementById("navbar__list"); // empty unordered list
+const navBarMenuSections = [...document.querySelectorAll("section")]; // creates an array and add items to it for each section in the document
 let navBarMenuItems = navBarMenuSections.length;
-const listSections = document.querySelectorAll('section'); // section elements
-const listlinks = document.querySelectorAll('.navbar__menu a'); // links in the navbar
+const listSections = document.querySelectorAll("section"); // section elements
+const listlinks = document.querySelectorAll(".navbar__menu a"); // links in the navbar
 /**
  * End Global Variables
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
 
-const dynamicNavBar = function(){ // function to dynamically add items to the nav bar list
+const dynamicNavBar = () => {
+    // function to dynamically add items to the nav bar list
     for (navBarMenuSection of navBarMenuSections) {
-        navBarMenuSectionName = navBarMenuSection.getAttribute('data-nav');
-        navBarMenuSectionLink = navBarMenuSection.getAttribute('id');
-        navBarMenuListItem = document.createElement('li');
+        navBarMenuSectionName = navBarMenuSection.getAttribute("data-nav");
+        navBarMenuSectionLink = navBarMenuSection.getAttribute("id");
+        navBarMenuListItem = document.createElement("li");
         navBarMenuListItem.innerHTML = `<a class='menu__link' href='#${navBarMenuSectionLink}'>${navBarMenuSectionName}</a>`;
         navBarMenu.appendChild(navBarMenuListItem);
-    };
+    }
 };
 
 // Add class 'active' to section when near top of viewport
 
-const sectionInViewport = function(view){ // determine if section is near top of viewport
+const sectionInViewport = (view) => {
+    // determine if section is near top of viewport
     let sectionxy = view.getBoundingClientRect();
-    return(sectionxy.top >= 0);
+    return sectionxy.top <= 150 && sectionxy.bottom >= 150;
 };
 
-const addActiveClass = function(){ // function to add active class to viewed section
-    for (navBarMenuSection of navBarMenuSections){
-        if (sectionInViewport(navBarMenuSection)){
-            if (!navBarMenuSection.classList.contains('your-active-class')){
-                navBarMenuSection.classList.add('your-active-class');
-            };
+const addActiveClass = () => {
+    // function to add active class to viewed section
+    for (navBarMenuSection of navBarMenuSections) {
+        if (sectionInViewport(navBarMenuSection)) {
+            if (!navBarMenuSection.classList.contains("your-active-class")) {
+                navBarMenuSection.classList.add("your-active-class");
+            }
         } else {
-            navBarMenuSection.classList.remove('your-active-class');
-        };
-    };
+            navBarMenuSection.classList.remove("your-active-class");
+        }
+    }
 };
 
 // Scroll smoothly to section on anchor click
 
-const smoothScroll = function(){
-    document.querySelectorAll('.menu__link').forEach(anchor => { // selects all anchors with class='menu__link'
-        anchor.addEventListener('click', function (e) {
+const smoothScroll = () => {
+    document.querySelectorAll(".menu__link").forEach((anchor) => {
+        // selects all anchors with class='menu__link'
+        anchor.addEventListener("click", function (e) {
             e.preventDefault();
-            document.querySelector(anchor.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
+            document.querySelector(anchor.getAttribute("href")).scrollIntoView({
+                behavior: "smooth",
             });
         });
     });
@@ -79,8 +83,8 @@ const smoothScroll = function(){
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
 // Build menu
 
@@ -92,6 +96,6 @@ smoothScroll();
 
 // Set sections as active
 
-document.addEventListener('scroll', addActiveClass);
+document.addEventListener("scroll", addActiveClass);
 
 /* End of Code :) */
